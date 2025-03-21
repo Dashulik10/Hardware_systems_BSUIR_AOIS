323b7e7b2f0c9c1318bd237faea381901661f3a9
Dashulik10 <144939580+Dashulik10@users.noreply.github.com>
2025-03-21 20:38:41 +0300

    Update normal_forms.py

--- LAB_2/logical_processing/normal_forms.py
+++ LAB_2/logical_processing/normal_forms.py
@@ -6,19 +6,23 @@ class NormalForms:
     def compute(self):
         sknf, sdnf = [], []
         sknf_indices, sdnf_indices = [], []
-        for index, (values, result) in enumerate(self.truth_table):
+
+        for index, (variable_values, _, final_result) in enumerate(self.truth_table):
             term = []
-            if result:
+            if final_result:
                 sdnf_indices.append(index)
                 for var in self.variables:
-                    term.append(var if values[var] else f'!{var}')
+                    term.append(var if variable_values[var] else f'!{var}')
                 sdnf.append(f"({' & '.join(term)})")
             else:
                 sknf_indices.append(index)
                 for var in self.variables:
-                    term.append(f'!{var}' if values[var] else var)
+                    term.append(f'!{var}' if variable_values[var] else var)
                 sknf.append(f"({' | '.join(term)})")
+
         return {
-            "СКНФ": " & ".join(sknf), "СДНФ": " | ".join(sdnf),
-            "СКНФ Индексы": sknf_indices, "СДНФ Индексы": sdnf_indices
+            "СКНФ": " & ".join(sknf),
+            "СДНФ": " | ".join(sdnf),
+            "СКНФ Индексы": sknf_indices,
+            "СДНФ Индексы": sdnf_indices
         }
